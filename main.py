443e77386f256725cbba8f06efe3ebeb8db8c8cb
Lucy Molnar <lmolnar24@biomedscienceacademy.org>
2021-01-21 23:58:35 +0000

    I created some questions.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 Chatbot
-Author: 
-Period/Core: 
+Author: Lucy Molnar 
+Period/Core: 2
 
 """
 
@@ -24,7 +24,51 @@ def run_tests():
 
 def main():
   """This function contains all code for the chatbot."""
-  print("Hello!")
+  print("Hello there i'm Lucy Molnar and I am a student at a local college and I need to interview some people for a project I was hoping I could ask you some questions.")
+
+print(" ")
+
+print("Ok why don't I start by asking you some questions.")
+
+print(" ")
+
+Qestion1 = input("Ok question one whats your name?")
+print("What an awesome name nice to meet you!")
+
+Question2 = input("Next whats your zodiac sign?")
+print("Thats so cool!")
+
+Question3 = input("Third question are you afraid of snakes?")
+if (Question3 == "yes" or Question3 == "yep"):
+  print("It's ok i'm afraid of them too! They are sooo scary!")
+elif (Question3 == "no" or Question3 == "nope"):
+  print("Yeah i'm not afraid of them either there not that scary.")
+else:
+  print("I used to be afraid of them but not anymore.")
+
+Question4 = input("Ok one more question after this Are you a dog or a cat person?")
+if (Question4 == "dog" or Question4 == "puppy"):
+  print("Thats so cool me too I love dogs so much I actually have two of my own!")
+elif(Question4 == "cat" or Question4 == "kitty"):
+  print("Wow me too! I have always been a cat person I happen to own three")
+else:
+  print("Oh cool I think both are great.")
+
+  Question5 = input("Ok now for the last question. What color are your eyes?")
+  if (Question5 == "brown" or Question5 == "Blue"):
+    print("Thats so cool! I wish my eyes were that color!")
+  elif(Question5 == "Green" or Question5 == "Hazel"):
+    print("Wow no fair I wish my eyes could be that color!")
+  else:
+    print("Omg what a unique color! I wish my eyes were that color!")
+
+Question6 = input("Oops wait sorry I have just one more small question to ask if you wouldn't mind... Did you enjoy this interview? I worked really hard on coming up with these questions.")
+if (Question6 == "yes" or Question6 == "yeah"):
+  print("Thank you so much I enjoyed talking to you too! Have a nice day!")
+elif(Question6 == "no" or Question6 == "nope"):
+  print("Oh well I am sorry to hear that...well thank you for letting me interview hope you have a nice day!")
+
+
 
 
 if __name__ == "__main__":
